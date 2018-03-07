8f9eb4d0321f819de4c21727519fd9290707350d
gq602735 <qgarnier17@gmail.com>
2018-03-07 11:02:23 +0100

    Splash

---
+++ app/src/main/java/e/nydaniem/projet_android/SplashScreen.java
@@ -0,0 +1,31 @@
+package e.nydaniem.projet_android;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gq602735 on 07/03/18.
+ */
+
+public class SplashScreen extends Activity {
+
+    private static int SPLASH_TIME_OUT = 10000;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
+    }
+}
